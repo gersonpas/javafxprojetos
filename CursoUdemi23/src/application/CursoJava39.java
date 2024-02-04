659b26e55a3c15ec2b2fa368f2ab32eafe13df1c
gorse <gorse@192.168.100.117>
2024-02-04 20:42:41 -0300

    Commit inicial Projetos com programas em JavaFx do curso da Udemy

---
+++ CursoUdemi23/src/application/CursoJava39.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+public class CursoJava39 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+		/*
+		 * double desconto; if (preco<20) { desconto = preco * 0.1; } else { desconto =
+		 * preco * 0.05; }
+		 */
+
+		System.out.println(desconto);
+		sc.close();
+
+	}
+
+}
---
+++ CursoUdemi23/src/application/Pessoa.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+	private String nome;
+	private LocalDate dataNascimento;
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+
+	public Pessoa(String nome, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
